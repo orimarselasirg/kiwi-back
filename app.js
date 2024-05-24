1bf3a128290c54ba233d227a6368bbcee3b41ef1
Ramiro Grisales noguera <ramirogrisales@gmail.com>
2024-05-24 11:59:08 -0500

    fix: route api vesion

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { Server } = require('socket.io');
-const http = require('http')
-const server = http.createServer(app)
+const { Server } = require("socket.io");
+const http = require("http");
+const server = http.createServer(app);
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const auth = require("./routes/auth");
-const services = require('./routes/service');
-const license = require('./routes/license');
-const client = require('./routes/client');
-const user = require('./routes/user');
-const tech = require('./routes/tech');
-const product = require('./routes/product');
-const station = require('./routes/station');
-const types = require('./routes/types.routes');
+const services = require("./routes/service");
+const license = require("./routes/license");
+const client = require("./routes/client");
+const user = require("./routes/user");
+const tech = require("./routes/tech");
+const product = require("./routes/product");
+const station = require("./routes/station");
+const types = require("./routes/types.routes");
 const io = new Server(server, {
-    cors: {
-        origin:'http://localhost:3000'
-    }
-})
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // app.use(cors(
 //     {
@@ -33,14 +33,14 @@ app.use(cors())
 //     }
 // ))
 
-app.use("/", auth);
-app.use("/", services);
-app.use("/", license);
-app.use("/", client);
-app.use("/", user);
-app.use("/", tech);
-app.use("/", product);
-app.use("/", station);
-app.use("/", types);
+app.use("/api/v1", auth);
+app.use("/api/v1", services);
+app.use("/api/v1", license);
+app.use("/api/v1", client);
+app.use("/api/v1", user);
+app.use("/api/v1", tech);
+app.use("/api/v1", product);
+app.use("/api/v1", station);
+app.use("/api/v1", types);
 
-module.exports = {server, io};
+module.exports = { server, io };
